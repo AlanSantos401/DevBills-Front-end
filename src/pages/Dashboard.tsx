33ec7b08604551eb8f2c44d1530a4574e26d866c
Alan <alandsantos54@gmail.com>
2025-07-04 14:35:36 -0300

    DevBills - Aula 17

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,22 @@
-
+import { useEffect } from "react";
+import { api } from "../services/api";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
+	useEffect(() => {
+		async function getTransactions() {
+			const response = await api.get("/transactions");
+
+			console.log(response);
+		}
+
+		getTransactions();
+	}, []);
+
+	return (
+		<div>
+			<h1>Dashboard</h1>
+		</div>
+	);
+};
 
 export default Dashboard;
