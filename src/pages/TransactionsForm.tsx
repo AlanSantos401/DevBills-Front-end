f3db6c227689ff44a6a4dc6d5e80c84573b09166
Alan <alandsantos54@gmail.com>
2025-07-29 10:52:39 -0300

    Atualização

---
+++ src/pages/TransactionsForm.tsx
@@ -0,0 +1,9 @@
+const TransactionsForm = () => {
+	return (
+		<div>
+			<h1>transações</h1>
+		</div>
+	);
+};
+
+export default TransactionsForm;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,9 @@ import { AuthProvider } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard";
 import PrivateRoutes from "./PrivateRoutes";
 import AppLayout from "../layout/AppLayout";
+import Transactions from "../pages/Transactions";
+import TransactionsForm from "../pages/TransactionsForm";
+
 
 const AppRoutes = () => {
 	return (
@@ -17,6 +20,8 @@ const AppRoutes = () => {
 					<Route element={<PrivateRoutes />}>
 						<Route element={<AppLayout />}>
 							<Route path="/dashboard" element={<Dashboard />} />
+							<Route path="/transacoes" element={<Transactions />} />
+							<Route path="/transacoes/nova" element={<TransactionsForm />} />
 						</Route>
 					</Route>
 
