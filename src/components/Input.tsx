b406142b2cb1a28d01ed3fafdeee7cdf2fd40ec6
Alan <alandsantos54@gmail.com>
2025-08-16 10:46:22 -0300

    Atualização

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -39,7 +39,7 @@ const Input = ({
 			</div>
 			<input
 				id={inputId}
-				className={`block w-full rounded-xl border ${error ? "border-red-500" : "border-gray-700"}
+				className={`no-spinner block w-full rounded-xl border ${error ? "border-red-500" : "border-gray-700"}
             bg-gray-800 px-4 py-3 text-sm text-gray-50
             transition-all focus:outline-none focus:ring-2
             ${error ? "focus:border-red-500 focus:ring-red-500/2" : "focus:border-primary-500 focus:ring-primary-500/2"}
